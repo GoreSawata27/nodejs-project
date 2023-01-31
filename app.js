56855f52a569e3844cead7ddf4925c3521043ea4
sawata gore <sawatagore27@gmail.com>
2023-01-31 13:27:15 +0530

    hi

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require("http");
 const server = http.createServer((req, res) => {
   res.end("Hello World");
 });
-const port = 3000;
+const port = process.env.PORT || 3000;
 server.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
